555114c732431479e7d0c0ec83837742b9e18d3e
root <mngenge@gmail.com>
2023-02-21 22:57:23 -0800

    task 10

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -9,16 +9,14 @@ void quick_sort_hoare(int *array, size_t size);
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
- *//
+ */
 void swap_ints(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
-
 /**
  * hoare_partition - Order a subset of an array of integers
  *                   according to the hoare partition scheme.
@@ -34,26 +32,23 @@ void swap_ints(int *a, int *b)
  */
 int hoare_partition(int *array, size_t size, int left, int right)
 {
-	int pivot, above, below;
-
-	pivot = array[right];
-	for (above = left - 1, below = right + 1; above < below;)
-	{
-		do {
-			above++;
-		} while (array[above] < pivot);
-		do {
-			below--;
-		} while (array[below] > pivot);
-
-		if (above < below)
-		{
-			swap_ints(array + above, array + below);
-			print_array(array, size);
-		}
-	}
-
-	return (above);
+int pivot, above, below;
+pivot = array[right];
+for (above = left - 1, below = right + 1; above < below;)
+{
+do {
+above++;
+} while (array[above] < pivot);
+do {
+below--;
+} while (array[below] > pivot);
+if (above < below)
+{
+swap_ints(array + above, array + below);
+print_array(array, size);
+}
+}
+return (above);
 }
 
 /**
@@ -67,14 +62,13 @@ int hoare_partition(int *array, size_t size, int left, int right)
  */
 void hoare_sort(int *array, size_t size, int left, int right)
 {
-	int part;
-
-	if (right - left > 0)
-	{
-		part = hoare_partition(array, size, left, right);
-		hoare_sort(array, size, left, part - 1);
-		hoare_sort(array, size, part, right);
-	}
+int part;
+if (right - left > 0)
+{
+part = hoare_partition(array, size, left, right);
+hoare_sort(array, size, left, part - 1);
+hoare_sort(array, size, part, right);
+}
 }
 
 /**
@@ -88,8 +82,7 @@ void hoare_sort(int *array, size_t size, int left, int right)
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
-	hoare_sort(array, size, 0, size - 1);
+if (array == NULL || size < 2)
+return;
+hoare_sort(array, size, 0, size - 1);
 }
